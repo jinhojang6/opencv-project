37af7ae77add2118fa35fa2b58d61b233d1eb3ae
jinhojang6 <jinho5051@gmail.com>
2019-11-19 16:52:32 +0900

    add project2 references

---
+++ project_1/thresholding_opt1.py
@@ -0,0 +1,61 @@
+import sys
+import time
+import numpy as np
+import cv2
+print(sys.executable)
+print(sys.version)
+print(cv2.__version__)
+
+def evaluate_threshold(path, threshold):
+	cap = cv2.VideoCapture(video_file)
+	timeP = time.time()
+	diff_sum = 0
+	if cap.isOpened():
+		ret, img = cap.read()
+		while ret:
+			gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			ret, thresh_image = cv2.threshold(gray_image, threshold, 255, cv2.THRESH_BINARY)
+			M1 = np.sum(thresh_image) / 255
+			M0 = np.size(thresh_image) - M1
+			diff_sum += abs(M0 - M1)
+			ret, img = cap.read()
+
+	print(f'thresh {threshold}: {diff_sum}, elapsed time: {time.time() - timeP}s')
+	return diff_sum
+
+def evaluate_section(path, value_list = [-1] * 256, init = 0, end = 255, div = 4):
+	div = min(div, end - init)
+	thresh_list = [0] * (div + 1)
+	eval_list = [0] * (div + 1)
+	for index in range(div + 1):
+		threshold = init + ((end - init) * index // div)
+		thresh_list[index] = threshold
+		if value_list[threshold] < 0:
+			value_list[threshold] = evaluate_threshold(path, threshold)
+		eval_list[index] = value_list[threshold]
+
+	for index in range(div + 1):
+		if index == div:
+			index_min = index
+			break
+		if eval_list[index + 1] > eval_list[index]:
+			index_min = index
+			break
+
+	if div == (end - init):
+		return thresh_list[index_min], eval_list[index_min]
+	else:
+		return evaluate_section(path, value_list, thresh_list[max(0, index_min - 1)], thresh_list[min(div, index_min + 1)], div)
+
+video_file = "./data/butterflies.mp4"
+cap = cv2.VideoCapture(video_file)
+
+print(f'frame size: {cap.get(cv2.CAP_PROP_FRAME_WIDTH)} by {cap.get(cv2.CAP_PROP_FRAME_HEIGHT)}')
+print(f'frame count: {cap.get(cv2.CAP_PROP_FRAME_COUNT)}')
+print(f'pixel count: {cap.get(cv2.CAP_PROP_FRAME_WIDTH) * cap.get(cv2.CAP_PROP_FRAME_HEIGHT) * cap.get(cv2.CAP_PROP_FRAME_COUNT) / 1000000}M')
+
+timeI = time.time()
+thresh_opt, diff_opt = evaluate_section(video_file)
+
+print(f'total elapsed time: {time.time() - timeI}s')
+print(f'optimal threshold: {thresh_opt} at diff_sum {diff_opt}')
---
+++ project_2/references/real-time-webcam/README.md
@@ -0,0 +1,21 @@
+# Webcam Object Detection
+
+Real time object detection of stream coming through webcam.<br/>
+Create-react-app project with tensorflow.js.<br/>
+COCO-SSD model with lite_mobilenet_v2 cnn<br/>
+
+# How to run the app
+
+    git pull
+    npm install
+    npm start
+
+# Demo
+
+<img src="https://raw.githubusercontent.com/leartgjoni/webcam-object-detection/master/demo/demo.gif" />
+
+# Live Demo
+
+https://mighty-refuge-51184.herokuapp.com/
+
+# Don't forget to star this repo ;)
