ef1448f2f07e73b0c0079ff88ba5cdb9daae3a84
Orbis2358 <mgkid3310@naver.com>
2019-12-07 22:57:18 +0900

    Update analysis.py

--- project_2/analysis.py
+++ project_2/analysis.py
@@ -6,13 +6,13 @@ from test_default import test_default
 from test_improved import test_improved
 
 execution_path = os.getcwd()
-filepath = 'drone_01.mp4'
+filename = 'drone_01.mp4'
 
-words = filepath.split('.')
-filename = '.'.join(words[:len(words) - 1])
+words = filename.split('.')
+filename_short = '.'.join(words[:len(words) - 1])
 
-path_in = os.path.join(execution_path, 'videos\\', filepath)
-path_out = os.path.join(execution_path, 'results\\', filename)
+path_in = os.path.join(execution_path, 'videos\\', filename)
+path_out = os.path.join(execution_path, 'results\\', filename_short)
 
 test_default(path_in, path_out)
 test_improved(path_in, path_out)
