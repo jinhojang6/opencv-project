09cda7a866eb69cbf280e19419c08819fe7c7329
Orbis2358 <mgkid3310@naver.com>
2019-12-09 16:31:18 +0900

    get total statstics

--- project_2/analysis.py
+++ project_2/analysis.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 from test_default import test_default
 from test_improved import test_improved
+import analysis_stastics
 
 execution_path = os.getcwd()
 filename = 'PopeVisitToKorea.mp4'
@@ -14,5 +15,8 @@ filename_short = '.'.join(words[:len(words) - 1])
 path_in = os.path.join(execution_path, 'videos\\', filename)
 path_out = os.path.join(execution_path, 'results\\', filename_short)
 
+analysis_stastics.stats = analysis_stastics.stastics()
 test_default(path_in, path_out)
 test_improved(path_in, path_out)
+
+print(analysis_stastics.stats.get_avg())
--- project_2/analysis_perframe.py
+++ project_2/analysis_perframe.py
@@ -1,18 +1,21 @@
 import os
+import analysis_stastics
 
 def per_frame_handler(frame_number, output_array, output_count = None, returned_frame = None, suffix = 'default'):
 	stat = {}
 
 	for output in output_array:
 		if output['name'] not in stat:
-			stat[output['name']] = {'count' : 0, 'confidence' : 0}
+			stat[output['name']] = {'count' : 0, 'confidence_sum' : 0, 'confidence' : 0}
 
 		stat[output['name']]['count'] += 1
-		stat[output['name']]['confidence'] += output['percentage_probability']
+		stat[output['name']]['confidence_sum'] += output['percentage_probability']
 
 	for item in stat:
 		if stat[item]['count'] > 0:
-			stat[item]['confidence'] = stat[item]['confidence'] / stat[item]['count']
+			stat[item]['confidence'] = stat[item]['confidence_sum'] / stat[item]['count']
+
+	analysis_stastics.stastics.add_stat(suffix, stat)
 
 	report_path = f'./results/analysis_{suffix}.txt'
 	file_mode = 'w'
